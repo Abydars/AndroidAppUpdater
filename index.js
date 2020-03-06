56b86ef1b28b7fdae962016f419e0562058148a0
Abid Raza <abidr.w@gmail.com>
2020-03-07 03:30:58 +0500

    update

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var apkUpdater = require('apk-updater');
 var app = express()
 
 apkUpdater.enable(app, '/anyUpdateServerRoute');
+apkUpdater.enable(app, '/anyUpdateServerRoute', 'apks');
 
 const port = 5000;
 const server = app.listen(port, function () {
